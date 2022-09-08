14a78d4784ea92dcf137802dcdb3603be8f8cb0e
Cyrus <daniellongwe800@gmail.com>
2022-09-08 15:17:45 -0700

    bites

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long longlongintType;
 	long int longintType;
 
-	puts("Size of a char:%zu byte(s)", sizeof(charType));
-	puts("Size of an int:%zu byte(s)", sizeof(intType));
-	puts("Size of a long int:%zu byte(s)", sizeof(longintType));
-	puts("Size of a long long int: %zu byte(s)", sizeof(longlongintType));
-	puts("Size of a float: %zu byte(s)", sizeof(floatType));
+	printf("Size of a char:%zu byte(s)\n"sizeof(charType));
+	printf("Size of an int:%zu byte(s)\n"sizeof(intType));
+	printf("Size of a long int:%zu byte(s)\n"sizeof(longintType));
+	printf("Size of a long long int:%zu byte(s)\n"sizeof(longlongintType));
+	printf("\nSize of a float:%zu byte(s)"sizeof(floatType));
 
 	return (0);
 }
