142c628938532e00da31af9e8be5511f0aa73eb2
Cyrus <daniellongwe800@gmail.com>
2022-09-09 17:59:41 -0700

    loop

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'e')
+			for (x = 'f'; x <= 'p'; x++)
+				
+				
+				putchar(x);
+			
+	}
+	putchar('\n');
+	return (0);
+
+}
