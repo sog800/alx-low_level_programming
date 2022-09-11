1d29c09e742bcf213936a5fb18dcda5f0ead5642
Cyrus <daniellongwe800@gmail.com>
2022-09-11 01:35:00 -0700

    aphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print_tebahpla
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	       putchar(x);
+	
+	putchar('\n');
+	return (0);	
+}
