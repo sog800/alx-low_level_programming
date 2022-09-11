8991cdf14676cf4f385aab4000a9ff6c94f001bc
Cyrus <daniellongwe800@gmail.com>
2022-09-10 23:45:04 -0700

    numbers with putchar

---
+++ 0x01-variables_if_else_while/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a string to standard error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" -");
+	fprintf("\rDora Korpar, 2015-10-19\n");
+}
