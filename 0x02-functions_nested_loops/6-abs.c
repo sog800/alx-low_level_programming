b15f149dec123a31d5bde3956cff0be660ef9041
Cyrus <daniellongwe800@gmail.com>
2022-09-13 14:50:15 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+ * _abs - printing absolute number
+ * @x: printing abolute number of x
+ *
+ * Description: printing absolute number
+ * Return: Always Returning an int
+ */
+int _abs(int x)
+{
+	int y;
+
+	y = abs(x);
+	return (y);
+}
