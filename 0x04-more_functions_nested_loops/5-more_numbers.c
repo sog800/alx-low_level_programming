db1a8fef5fb29b76f324f79aea63bc01683724e2
Cyrus <daniellongwe800@gmail.com>
2022-09-15 12:33:50 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing numbers
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (y = 0; y <= 14; x++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
