f75b33deb38e58e179f512111ff051ed02cdf617
Cyrus <daniellongwe800@gmail.com>
2022-09-15 16:02:39 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,23 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (x != 0)
+		if (x % 15 == 0)
 		{
-			if (x % 15 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-			else if (x % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else if (x % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("%d ", x);
-			}
+			printf("FizzBuzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
 		}
 	}
 	putchar('\n');
