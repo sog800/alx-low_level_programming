f741f9b081bfed4dbf8a06c2c4718a22097c274f
Cyrus <daniellongwe800@gmail.com>
2022-09-15 13:58:10 -0700

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,9 @@ void more_numbers(void)
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = 0; y <= 14; x++)
+		for (y = 0; y <= 14; y++)
 		{
-			_putchar(y + '0');
+			_putchar(y % 10 + '0');
 		}
 		_putchar('\n');
 	}
