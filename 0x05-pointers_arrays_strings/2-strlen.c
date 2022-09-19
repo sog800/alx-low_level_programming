81e3d3d27331677ed9adeb54a901c455e8ea4709
Cyrus <daniellongwe800@gmail.com>
2022-09-19 03:45:13 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - lenth of a string
+ * @s: parameter of the string to be tasted
+ *
+ * Return: always int for success
+ */
+int _strlen(char *s)
+{
+	int lenNumber;
+
+	lenNumber = strlen(s);
+
+	return (lenNumber);
+}
