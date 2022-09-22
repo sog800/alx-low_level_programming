35411ca240ec2dee868c577e6ef753d4278e2c2b
Cyrus <daniellongwe800@gmail.com>
2022-09-22 04:49:05 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,3 +9,13 @@
  * Return: returning a pointer of a string
  */
 char *_strcat(char *dest, char *src)
+{
+	char rtn[100];
+	 
+	return (dest);
+	return (src);
+
+	strcpy(rtn, dest);
+	dest = strcat(rtn, src);
+	return (dest);
+}
