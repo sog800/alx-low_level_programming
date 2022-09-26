6c413def084944325b38b43241cf12d975f251f2
Cyrus <daniellongwe800@gmail.com>
2022-09-26 16:37:29 -0700

    match

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+#include <stdbool.h>
+
+/**
+ * _strspn - strspn
+ * @s: string to be scarnned
+ * @accept: string to comparere
+ *
+ * Return: an int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	size_t ilen = 0;
+	size_t len1, len2;
+	size_t i, j;
+	bool found;
+
+	len1 = strlen(s);
+	len2 = strlen(accept);
+
+	for (i = 0; i < len1; i++)
+	{
+		found = false;
+		for (j = 0; j < len2; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				found = true;
+				break;
+			}
+		}
+		if (!found)
+			break;
+		else
+			ilen++;
+	}
+	return (ilen);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ int _putchar(char c);
 void set_string(char **s, char *to);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif /*MAIN_H*/
