420ba4e3406fb5a332026179c9f6d1ddf1481b4c
Cyrus <daniellongwe800@gmail.com>
2022-09-26 13:12:35 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,9 +13,16 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int x;
 
-	for (x = 0; x < n; x++)
+	for (x = 0; x <= n; x++)
 	{
-		dest[x] = src[x];
+		if (x == 5 || x == 6)
+		{
+			dest[x] = src[6];
+		}
+		else
+		{
+			dest[x] = src[x];
+		}
 	}
 	return (0);
 }
