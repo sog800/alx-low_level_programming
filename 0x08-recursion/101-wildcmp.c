f058bc197f6a48db389da802467499a3664c9b30
Cyrus <daniellongwe800@gmail.com>
2022-09-28 17:06:52 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,5 @@
 #include "main.h"
 #include <string.h>
-#include <stdio.h>
-char *string2(char *s);
 /**
  * wildcmp - comparing strings
  * @s1: string 1
@@ -12,14 +10,17 @@ char *string2(char *s);
 int wildcmp(char *s1, char *s2)
 {
 	int l1, l2;
+	int x = 0;
 
 	l1 = strlen(s1);
 	l2 = strlen(s2);
 	if (!(s2[0] == '*' && s2[l2 - 1] == s1[l1 - 1]))
 	{
-		string2(s2);
-		printf("%s\n", s2);
-		if (s1[0] == s2[0] && s1[l1 - 1] == s2[l2 - 1])
+		if (s2[x] == '*')
+		{
+			x++;
+		}
+		if (s1[x] == s2[x] && s1[l1 - 1] == s2[l2 - 1])
 		{
 			return (1);
 		}
@@ -41,34 +42,4 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	}
 
-}
-/**
- * string2 - removing char * in s2
- * @s: string
- *
- * Return: s
- */
-char *string2(char *s)
-{
-	int x = 0;
-
-	if (s[x] == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		if (s[x] == '*')
-		{
-			memmove(&s[x], &s[x + 1], strlen(s) - x);
-			x++;
-		}
-		else
-		{
-			s[x] = s[x];
-			x++;
-		}
-		string2(s);
-	}
-	return (s);
 }
