4b7d82588e23a00e6398e75a7421d00823b15629
Cyrus <daniellongwe800@gmail.com>
2022-10-10 12:00:53 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ * @name: character
+ * @age: floating point
+ * @owner: owner
+ *
+ * Description: dog struture
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
