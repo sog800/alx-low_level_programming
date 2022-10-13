9173fcb8ca05a670a93619aa4bfe1e100e6f21d8
Cyrus <daniellongwe800@gmail.com>
2022-10-13 00:30:02 -0700

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,41 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - index
+ * @array: array
+ * @size: number of elements
+ * @cmp: function pointer
+ *
+ * Return: Always return an integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x, rtn;
+
+	if (array == NULL || cmp == NULL)
+	{
+		exit(1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = 0;
+		while (x < size)
+		{
+			rtn = cmp(array[x]);
+			if (rtn != 0)
+			{
+				return (x);
+				break;
+			}
+			x++;
+			if (x == size)
+				return (-1);
+		}
+	}
+	return (0);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
-#include <stddef.h> 
+#include <stddef.h>
+int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
