ba8542788e4911fa0cd3cd147a9cb0a9e86050b6
Cyrus <daniellongwe800@gmail.com>
2022-10-21 01:44:43 -0700

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * myStartup - print before main
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
