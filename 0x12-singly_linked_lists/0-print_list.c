16df6be3de0875a995d337074d29c687cb776545
Cyrus <daniellongwe800@gmail.com>
2022-10-21 03:17:19 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,16 +11,25 @@ size_t print_list(const list_t *h)
 	size_t nod;
 	const list_t *ptr;
 
-	ptr = NULL;
 	if (h == NULL)
 	{
-		printf("[0] (nil)\n");
+		nod = 0;
+		return (nod);
 	}
+	ptr = NULL;
 	ptr = h;
 	nod = 0;
 	while (ptr != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u]", ptr->len);
+			printf("%s\n",ptr->str);
+		}
 		ptr = ptr->next;
 		nod++;
 	}
