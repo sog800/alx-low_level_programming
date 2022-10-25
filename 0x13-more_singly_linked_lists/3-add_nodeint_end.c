0767917d3783df3a2e10ac81a9721f6b347d270e
Cyrus <daniellongwe800@gmail.com>
2022-10-25 02:17:22 -0700

    add at the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adding at the end of the list
+ * @head: head pointer
+ * @n: integer data of the list
+ *
+ * Return: apointer heat of the head
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *tmp;
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	}
+		return (NULL);
+	}
+	ptr->n = n;
+	ptr->next = NULL;
+	if (*head == NULL)
+	{
+		*head = ptr;
+		return (*head);
+	}
+	*head = ptr;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = ptr;
+	return (*head);
+
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,7 @@
 #define _LISTS_H_
 
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -18,4 +18,5 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 #endif /*_LISTS_H_*/
