25f212e795f99b71b270a9ee48baa039f1097b8b
Cyrus <daniellongwe800@gmail.com>
2022-11-01 14:35:15 -0700

    conversion

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * binary_to_uint - binary to int
+ * @b: string of binary number
+ *
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len, i;
+	unsigned int total;
+	unsigned int decval;
+
+	len = strlen(b);
+	total = 0;
+	decval = 1;
+	if (b == NULL)
+		return (0);
+	for (i = (len -1); i >= 0; i--)
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			total = 0;
+			return (total);
+			exit (0);
+		}
+	}
+	for (i = (len - 1); i >= 0; i--)
+	{
+		if (b[i] == '1')
+			total += decval;
+		decval *= 2;
+	}
+	return (total);
+}
